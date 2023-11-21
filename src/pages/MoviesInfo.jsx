16a5bca1d8198c75a3d893eb8f6a02f1da3b21ed
jorgerobayo <jorgerobmor@gmail.com>
2023-11-20 22:22:27 -0500

    key almost set

--- src/pages/MoviesInfo.jsx
+++ src/pages/MoviesInfo.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function MoviesInfo(props) {
-  
+    const apiKey = 'd0aed252'
   return (
     <h1>MoviesInfo Component</h1>
   )
